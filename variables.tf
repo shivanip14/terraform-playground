0d001c77b5d92905543b6edc609a03a55a3b6390
Shivani <92314538+shivanip14@users.noreply.github.com>
2024-03-11 14:51:33 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,23 @@
+variable "RGName" {
+  type = string
+}
 
+variable "location" {
+  type = string
+}
+
+variable "subscriptionID" {
+  type = string
+}
+
+variable "clientID" {
+  type = string
+}
+
+variable "clientSecret" {
+  type = string
+}
+
+variable "tenantID" {
+  type = string
+}
