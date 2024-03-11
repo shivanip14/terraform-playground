135ce41554e07233c41115cf9f6bc9304be35fec
Shivani <92314538+shivanip14@users.noreply.github.com>
2024-03-11 14:56:57 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,15 @@
+provider "azurerm" {
+  version = "<=2.0.0"
 
+  subscription_id = var.subscriptionID
+  client_id = var.clientID
+  client_secret = var.clientSecret
+  tenant_id = var.tenantID
+
+  features {}
+}
+
+resource "azurerm_resource_group" "tfrg_name" {
+  name = var.RGName
+  location = var.location
+}
