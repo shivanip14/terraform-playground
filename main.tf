d42f1639a7fab61dc6db79661c832c2c2062ee3b
Shivani <92314538+shivanip14@users.noreply.github.com>
2024-03-11 18:27:24 +0100

    Added module to main.tf

--- main.tf
+++ main.tf
@@ -9,6 +9,13 @@ provider "azurerm" {
   features {}
 }
 
+module "storage_account" {
+  source = "./modules/sta"
+  location = var.location
+  storage_account_name = var.STAName
+  resource_group_name = var.RGName
+}
+
 resource "azurerm_resource_group" "tfrg_name" {
   name = var.RGName
   location = var.location
