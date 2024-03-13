ea60f675e9331e148d419d64fdc78e488bfda0b2
Shivani <92314538+shivanip14@users.noreply.github.com>
2024-03-13 18:18:25 +0100

    Added helper comment

--- modules/rg/resourcegroup.tf
+++ modules/rg/resourcegroup.tf
@@ -1,6 +1,10 @@
 variable "location" {}
 variable "RGName" {}
 
+# The second argument below ("tfrg_name") is an identifier in this TF project for this resource group.
+# It is a logical name that can be used to refer to the RG's properties somewhere else,
+# and has no impact on the actual name of the RG in Azure.
+
 resource "azurerm_resource_group" "tfrg_name" {
   name = var.RGName
   location = var.location
